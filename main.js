6af01711ebd63c7e6fc2d4ce269566773f63956d
audu2110 <91955066+audu2110@users.noreply.github.com>
2022-12-19 23:26:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -162,3 +162,81 @@ function removeUserFromScreen(uid){
   }
   
 }
+
+
+
+// Update the user Details
+
+const myForm = document.querySelector('#my-form');
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  var name=event.target.usrName.value;
+  var email=event.target.emailid.value;
+  
+  
+    let obj={
+        name,
+        email
+    };
+    
+    axios.post("https://crudcrud.com/api/ee8021362d0341d4bdabb0d17f501b63/AppiontmentData",obj).then((response)=>{
+      showNewUserOnScreen(response.data)  
+    console.log(response)
+    }).catch((err)=>{
+      console.log(err);
+    })
+    
+}
+window.addEventListener("DOMContentLoaded", () =>{
+
+  axios.get("https://crudcrud.com/api/ee8021362d0341d4bdabb0d17f501b63/AppiontmentData").then((response)=>{
+    showNewUserOnScreen(response.data)  
+    for(var i=0;i<response.data.length;i++){
+      showNewUserOnScreen(response.data[i])
+    }
+      console.log(response)
+    }).catch((err)=>{
+      console.log(err);
+  })
+
+})
+function showNewUserOnScreen(user){
+  if(localStorage.getItem(user.email) !== null){
+    removeUserFromScreen(email);
+  }
+  const parentNode=document.getElementById('listOfUsers');
+  const childHTML = `<li id=${user._id}> ${user.name} - ${user.email}
+                                        <button onclick=deleteUser('${user._id}')> Delete User </button>
+                                        <button onclick=editUserDetails('${user._id}','${user.name}','${user.email}')>Edit User </button>
+                                     </li>`
+  parentNode.innerHTML=parentNode.innerHTML+childHTML;
+}
+
+function editUserDetails(uid, name,emailId){
+
+  document.getElementById('email').value = emailId;
+  document.getElementById('name').value = name;
+  deleteUser(uid)
+}
+
+function deleteUser(uid){
+  axios.delete(`https://crudcrud.com/api/ee8021362d0341d4bdabb0d17f501b63/AppiontmentData/${uid}`).then((response)=>{    
+    removeUserFromScreen(uid)
+    }).catch((err)=>{
+      console.log(err);
+    })
+  console.log(uid)
+  
+  removeUserFromScreen(uid);
+}
+
+function removeUserFromScreen(uid){
+  const parentNode = document.getElementById('listOfUsers');
+  const childNodeToBeDeleted = document.getElementById(uid);
+  if(childNodeToBeDeleted){
+    parentNode.removeChild(childNodeToBeDeleted);
+  }
+  
+}
